8c3c928941ae05d0a546685548d866a756837a87
LeeHyeJun <72326300+lee123-jun@users.noreply.github.com>
2024-09-09 16:18:16 +0900

    재난안전프로그래밍_2주차
    R 버전확인 ,산술연산자, 비교연산자, 비교연산자, 타입확인

---
+++ Week_2.R
@@ -0,0 +1,24 @@
+
+install.packages("installr")
+library(installr)
+
+
+check.for.updates.R()
+install.R()
+
+a <- 1
+a = 1
+1 ->> a 
+a = 10
+
+a <- 3
+b = 4
+c = 7
+d = a+b
+print(a)
+print(d)
+
+print(1)
+# 따옴표, 쌍다옴표: ',"로 문자 처음과 끝 표현
+print("hello world")
+print('hello world')
